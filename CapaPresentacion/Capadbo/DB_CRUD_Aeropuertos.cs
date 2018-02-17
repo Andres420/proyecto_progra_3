955a57a4ba6af8ce59e9bf68d52ac8487dabb1b0
RonaldP007 <paniaguarpg007@gmail.com>
2018-02-16 22:13:56 -0600

    prueba

--- CapaPresentacion/Capadbo/DB_CRUD_Aeropuertos.cs
+++ CapaPresentacion/Capadbo/DB_CRUD_Aeropuertos.cs
@@ -41,15 +41,18 @@ namespace Capadbo
             }
             string query = "SELECT a.id_aeropuerto,a.nombre_aeropuerto,l.nombre,a.iata from aeropuertos AS a"
                         + " INNER JOIN lugares AS l ON a.fk_idlugar = l.id_lugar;";
-            NpgsqlDataAdapter add = new NpgsqlDataAdapter(query, conexion);  
-            add.Fill(datos); 
-            dataGridView1.DataSource = datos.Tables[0]; 
+            NpgsqlDataAdapter add = new NpgsqlDataAdapter(query, conexion);
+            add.Fill(datos);
+            dataGridView1.DataSource = datos.Tables[0];
             dataGridView1.Columns[0].HeaderCell.Value = "ID";
             dataGridView1.Columns[1].HeaderCell.Value = "Nombre";
             dataGridView1.Columns[2].HeaderCell.Value = "Lugar";
             dataGridView1.Columns[3].HeaderCell.Value = "IATA";
             conexion.Close();
 
+        }
+        public void Modificar(int cedula)
+        {
         }
     }
 }

9ef84c7bc45078d70e58c93886cd26b618efda81
ANDRES ALBERTO AVILA BRENES <Enrique@KIKE>
2018-02-17 20:46:48 -0600

    Cargar datagrid con los lugares

--- CapaPresentacion/CapaNegocio/Codigo_CRUD_Lugares.cs
+++ CapaPresentacion/CapaNegocio/Codigo_CRUD_Lugares.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using Capadbo;
+using Npgsql;
 
 namespace CapaNegocio
 {
     public class Codigo_CRUD_Lugares
     {
+        string select = "SELECT id_lugar, nombre FROM lugares;";
         /// <summary>
         /// This method send the name of a place for other method
         /// </summary>
@@ -20,6 +24,23 @@ namespace CapaNegocio
             bool agregado = db_lugares.Agregar_Lugar(nombre_lugar);
             return agregado;
         }
+
+        public void Cargar_Data_Grid(DataGridView dataGridView1)
+        {
+                DB_CRUD_Hoteles db_hoteles = new DB_CRUD_Hoteles();
+                NpgsqlConnection conn = db_hoteles.Conexion();
+                conn.Open();
+                DataSet dataSet = new DataSet();
+                NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(select, conn);
+                adapter.Fill(dataSet, "Lugar");
+                dataGridView1.DataSource = dataSet.Tables[0];
+                dataGridView1.Columns[0].HeaderCell.Value = "Identificacion Lugar";
+                dataGridView1.Columns[1].HeaderCell.Value = "Nombre Lugar";
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                dataGridView1.Rows.RemoveAt((dataGridView1.RowCount - 1));
+                conn.Close();
+        }
+
         /// <summary>
         /// This method send information of a place for other method
         /// </summary>

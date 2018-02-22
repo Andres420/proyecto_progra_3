2da8faf55b7b9fb5e0300a2b4c4ac9322fd367d0
RonaldP007 <paniaguarpg007@gmail.com>
2018-02-22 10:36:00 -0600

    imagenes

--- CapaPresentacion/CapaPresentacion/Program.cs
+++ CapaPresentacion/CapaPresentacion/Program.cs
@@ -16,8 +16,8 @@ namespace CapaPresentacion
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
-            //Application.Run(new Alojamiento());
+            //Application.Run(new Login());
+            Application.Run(new CRUD_Paises());
         }
     }
 }

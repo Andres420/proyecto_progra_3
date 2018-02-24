062aa3fc49dd7a5075820606a40af97ad482ba2e
ANDRES ALBERTO AVILA BRENES <Enrique@KIKE>
2018-02-23 21:48:29 -0600

    cambios

---
+++ CapaPresentacion/CapaPresentacion/Busqueda_Auto.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace CapaPresentacion
+{
+    partial class Busqueda_Auto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnAgregar_Carro = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(543, 361);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseClick);
+            // 
+            // btnAgregar_Carro
+            // 
+            this.btnAgregar_Carro.Location = new System.Drawing.Point(587, 170);
+            this.btnAgregar_Carro.Name = "btnAgregar_Carro";
+            this.btnAgregar_Carro.Size = new System.Drawing.Size(101, 28);
+            this.btnAgregar_Carro.TabIndex = 1;
+            this.btnAgregar_Carro.Text = "Alquilar vehiculo";
+            this.btnAgregar_Carro.UseVisualStyleBackColor = true;
+            this.btnAgregar_Carro.Click += new System.EventHandler(this.btnAgregar_Carro_Click);
+            // 
+            // Busqueda_Auto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 385);
+            this.Controls.Add(this.btnAgregar_Carro);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Busqueda_Auto";
+            this.Text = "Busqueda_Auto";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnAgregar_Carro;
+    }
+}
---
+++ CapaPresentacion/Objetos/Pre_Compra_Vuelos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objetos
+{
+    public class Pre_Compra_Vuelos
+    {
+        public int id_vuelo { get; set; }
+        public string pais_origen { get; set; }
+        public string paisdestino { get; set; }
+        public DateTime fecha_inicio { get; set; }
+        public DateTime fecha_final { get; set; }
+        public int pasajeros { get; set; }
+        public int vehiculo { get; set; }
+
+        public Pre_Compra_Vuelos(int id_vuelo, string pais_origen, string paisdestino, DateTime fecha_inicio, DateTime fecha_final, int pasajeros, int vehiculo)
+        {
+            this.id_vuelo = id_vuelo;
+            this.pais_origen = pais_origen;
+            this.paisdestino = paisdestino;
+            this.fecha_inicio = fecha_inicio;
+            this.fecha_final = fecha_final;
+            this.pasajeros = pasajeros;
+            this.vehiculo = vehiculo;
+        }
+    }
+}
